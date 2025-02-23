be27f91f2d65574e9dacfc8ae7dc22d608d9bddf
Nahuel Segura <nahuel_segura_17@hotmail.com>
2025-02-22 21:21:49 -0300

    creacion de models para calculadora

---
+++ src/main/java/com/breakingns/ProyectoInteresCompuesto/model/Calculadora.java
@@ -0,0 +1,51 @@
+package com.breakingns.ProyectoInteresCompuesto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Calculadora {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id_calculo;
+    
+    private String nombre_calculo;
+    private Date fecha_calculo;
+    private Double inversion_inicial;
+    private Double contribucion_mensual;
+    private Integer cantidad_anios;
+    private Double tasa_interes_estimada;
+    private Double varianza_tasa_interes;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_capitalizacion")
+    private Capitalizacion capitalizacion;
+
+    public Calculadora() {
+    }
+
+    public Calculadora(Long id_calculo, String nombre_calculo, Date fecha_calculo, Double inversion_inicial, Double contribucion_mensual, Integer cantidad_anios, Double tasa_interes_estimada, Double varianza_tasa_interes, Capitalizacion capitalizacion) {
+        this.id_calculo = id_calculo;
+        this.nombre_calculo = nombre_calculo;
+        this.fecha_calculo = fecha_calculo;
+        this.inversion_inicial = inversion_inicial;
+        this.contribucion_mensual = contribucion_mensual;
+        this.cantidad_anios = cantidad_anios;
+        this.tasa_interes_estimada = tasa_interes_estimada;
+        this.varianza_tasa_interes = varianza_tasa_interes;
+        this.capitalizacion = capitalizacion;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/com/breakingns/ProyectoInteresCompuesto/model/Capitalizacion.java
@@ -0,0 +1,37 @@
+package com.breakingns.ProyectoInteresCompuesto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Capitalizacion {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id_capitalizacion;
+    private String nombre_capitalizacion;
+    
+    @OneToMany(mappedBy = "capitalizacion")
+    private List<Calculadora> calculo;
+
+    public Capitalizacion() {
+    }
+
+    public Capitalizacion(Long id_capitalizacion, String nombre_capitalizacion, List<Calculadora> calculo) {
+        this.id_capitalizacion = id_capitalizacion;
+        this.nombre_capitalizacion = nombre_capitalizacion;
+        this.calculo = calculo;
+    }
+    
+    
+
+}
